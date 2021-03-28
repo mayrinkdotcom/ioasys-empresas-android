d66ef6ea3a1196af10b9856e6de219ffbb9effbf
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-03-28 12:08:07 -0300

    Criando LoginActivity.kt e activity_login.xml

---
+++ app/src/main/java/br/com/ioasys/camp/empresas/LoginActivity.kt
@@ -0,0 +1,55 @@
+package br.com.ioasys.camp.empresas
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity () {
+    lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        button = findViewById(R.id.btnLogin)
+        Log.i("LoginActivity", "onCreate")
+
+        button.setOnClickListener {
+            Log.i("LoginActivity", "Botão clicado")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("LoginActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("LoginActivity", "LoginActivity entrou em onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("LoginActivity", "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("LoginActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("LoginActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("LoginActivity", "onDestroy")
+    }
+}
--- app/src/main/java/br/com/ioasys/camp/empresas/MainActivity.kt
+++ app/src/main/java/br/com/ioasys/camp/empresas/MainActivity.kt
@@ -2,10 +2,41 @@ package br.com.ioasys.camp.empresas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MainActivity", "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy")
     }
 }
