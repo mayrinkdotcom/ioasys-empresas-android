f9e35f078640473cd8565bd5d97c9462e5da42ea
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-04-03 13:11:11 -0300

    Fix Tela Home não exibe as empresas close #2

--- app/src/main/java/br/com/ioasys/camp/empresas/LoginFragment.kt
+++ app/src/main/java/br/com/ioasys/camp/empresas/LoginFragment.kt
@@ -1,7 +1,42 @@
 package br.com.ioasys.camp.empresas
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.ioasys.camp.empresas.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var button: AppCompatButton
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button = binding.btnLogin
+
+        button.setOnClickListener {
+            findNavController().setGraph(R.navigation.home_graph)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
