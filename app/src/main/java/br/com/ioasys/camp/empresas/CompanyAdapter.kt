46f3787a7a49cc73a3f8727d90c4288324114d4b
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-04-03 00:24:56 -0300

    Criando a fragment details, e as classes Details Fragment, Company e CompanyAdapter

---
+++ app/src/main/java/br/com/ioasys/camp/empresas/CompanyAdapter.kt
@@ -0,0 +1,52 @@
+package br.com.ioasys.camp.empresas
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class CompanyAdapter(private val callback: (Company) -> Unit): RecyclerView.Adapter<CompanyAdapter.CompanyViewHolder>(){
+
+    private var companies = emptyList<Company>()
+
+    fun setItems(list: List<Company>) {
+        companies = list
+        notifyDataSetChanged()
+    }
+
+    inner class CompanyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val ivImageCompany: AppCompatImageView = itemView.findViewById<AppCompatImageView>(R.id.imageCompany)
+        private val tvCompanyName: AppCompatTextView = itemView.findViewById<AppCompatTextView>(R.id.textCompanyName)
+        private val tvCompanyRole: AppCompatTextView = itemView.findViewById<AppCompatTextView>(R.id.textCompanyRole)
+        private val tvCompanyCountry: AppCompatTextView = itemView.findViewById<AppCompatTextView>(R.id.textCompanyCountry)
+
+        fun bind(company: Company) {
+            tvCompanyName.text = company.companyName
+            tvCompanyRole.text = company.companyType.companyTypeName
+            tvCompanyCountry.text = company.country
+            itemView.setOnClickListener { callback.invoke(company) }
+
+            Glide
+                .with(itemView)
+                .load(company.pathImage)
+                .placeholder(R.drawable.toolbar_background)
+                .into(ivImageCompany)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CompanyViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_company, parent, false)
+        return CompanyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CompanyViewHolder, position: Int) {
+        holder.bind(companies[position])
+    }
+
+    override fun getItemCount() = companies.size
+}
---
+++ app/src/main/java/br/com/ioasys/camp/empresas/DetailsFragment.kt
@@ -0,0 +1,61 @@
+package br.com.ioasys.camp.empresas
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import br.com.ioasys.camp.empresas.databinding.FragmentDetailsBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+class DetailsFragment : Fragment () {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+    private val args: DetailsFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configureView()
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        (activity as AppCompatActivity).run {
+            setSupportActionBar(binding.toolbarCompanyDetails)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        }
+    }
+
+    private fun configureView() {
+        binding.tvCompanyDescription.text = args.companyDescription
+        binding.tvCompanyName.text = args.companyName
+        Glide
+            .with(this)
+            .load(args.imageUrl)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
+            .into(binding.ivCompanyImage)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
