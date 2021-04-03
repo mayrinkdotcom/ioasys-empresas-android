975d9465875a9888a0450812d0e04c2173e142d8
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-04-02 23:59:46 -0300

    Atualizando Main Activity para hospedar as fragments dessa tela

--- app/src/main/java/br/com/ioasys/camp/empresas/MainActivity.kt
+++ app/src/main/java/br/com/ioasys/camp/empresas/MainActivity.kt
@@ -3,8 +3,13 @@ package br.com.ioasys.camp.empresas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
+import br.com.ioasys.camp.empresas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController by lazy { findNavController(R.id.host_fragment) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,5 +43,9 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.i("MainActivity", "onDestroy")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
