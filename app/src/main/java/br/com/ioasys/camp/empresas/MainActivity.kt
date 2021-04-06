4bb6039449573ce9881231bb9f5d826418f534ea
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-04-06 01:19:31 -0300

    Removendo logs desnecessários, adicionando view binding na MainActivity

--- app/src/main/java/br/com/ioasys/camp/empresas/MainActivity.kt
+++ app/src/main/java/br/com/ioasys/camp/empresas/MainActivity.kt
@@ -10,42 +10,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val navController by lazy { findNavController(R.id.host_fragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i("MainActivity", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("MainActivity", "onResume")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("MainActivity", "onRestart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("MainActivity", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("MainActivity", "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("MainActivity", "onDestroy")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp()
     }
 }
